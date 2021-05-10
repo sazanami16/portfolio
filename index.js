d47c4f74367decce90255836b23d4d151733f471
sazanami16 <za_nami.za_nami16@icloud.com>
2021-05-10 21:20:27 +0900

    スクロール検知のJSを記述

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+let fadeInContent = document.querySelectorAll('.js-slide')
+
+window.addEventListener('scroll', () => {
+  for (let i = 0; i < fadeInContent.length; i++) {
+    const rect = fadeInContent[i].getBoundingClientRect().top;
+    const scroll = window.pageYOffset || document.documentElement.scrollTop;
+    const offset = rect + scroll;
+    const windowHeight = window.innerHeight; 
+    if (scroll > offset - windowHeight + 150) {
+      fadeInContent[i].classList.add('scroll-in');
+    }
+  }
+
+});
 
